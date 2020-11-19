96d95586a911c6fcf4bf2f42923f5cebeb0012b1
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-19 21:58:27 +0100

    Changed @Valid to @Validated.

--- src/main/java/com/saimirgasa/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/saimirgasa/msscbeerservice/web/controller/BeerController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,13 +28,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
         //TODO impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
 
         //TODO: impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);

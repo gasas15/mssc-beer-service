9c255c8c66ee7d90fbbe6349d66875f5d6145416
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-19 10:44:29 +0100

    Removed unused import.

--- src/test/java/com/saimirgasa/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/saimirgasa/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,17 +1,15 @@
 package com.saimirgasa.msscbeerservice.web.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saimirgasa.msscbeerservice.web.model.BeerDto;
